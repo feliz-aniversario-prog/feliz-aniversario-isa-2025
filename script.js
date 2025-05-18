8fbb4576f11318a956a7e5f4cf60292c47c7b5c9
feliz-aniversario-prog <milenliveira24@gmail.com>
2025-05-18 12:32:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Função para exibir uma mensagem surpresa
 function surpriseMessage() {
-    alert("Você é a melhor amiga do mundo! Amo muito você, nossa amizade é incrível!");
+    alert("Você é muito importante para mim! Amo muito você, espero que eu veja você crescer mais e mais!");
 }
 
 // Iniciar música de feliz aniversário
