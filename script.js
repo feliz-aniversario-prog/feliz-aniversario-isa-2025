40f588fd34c74f1ec6340c53a4d117117ce52dbb
feliz-aniversario-prog <milenliveira24@gmail.com>
2025-05-18 12:40:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Função para exibir uma mensagem surpresa
 function surpriseMessage() {
     alert("Você é muito importante para mim! Amo muito você, espero que eu veja você crescer mais e mais!");
+    
+    // Tocar a música após o clique
+    document.getElementById('birthdaySong').play();
 }
 
 // Espera a página carregar completamente antes de executar qualquer código
 window.onload = () => {
-    // Tocar a música de Feliz Aniversário assim que a página for carregada
-    document.getElementById('birthdaySong').play();
-
     // Adicionar o evento ao botão para mostrar a surpresa
     document.getElementById('surpriseBtn').addEventListener('click', surpriseMessage);
 };
