b6f8ec5c140e7fbf0b562c932156c2becf242f11
feliz-aniversario-prog <milenliveira24@gmail.com>
2025-05-18 12:31:37 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para exibir uma mensagem surpresa
+function surpriseMessage() {
+    alert("Você é a melhor amiga do mundo! Amo muito você, nossa amizade é incrível!");
+}
+
+// Iniciar música de feliz aniversário
+window.onload = () => {
+    document.getElementById('birthdaySong').play();
+};
